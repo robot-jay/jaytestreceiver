f7335420b053ab47bfb9b4655f0c104a2ddc13ef
Jay Ragsdale <jay@mtstesters.com>
2026-07-03 14:20:11 -0500

    Simplifying for debugging reasons.

--- src/functions/receiveTestEvent.ts
+++ src/functions/receiveTestEvent.ts
@@ -1,17 +1,4 @@
 import { app, InvocationContext } from "@azure/functions";
-import sql from "mssql";
-
-const sqlConfig: sql.config = {
-  server: process.env.SQL_SERVER!,
-  database: process.env.SQL_DATABASE!,
-  options: {
-    encrypt: true,
-  },
-  authentication: {
-    type: "azure-active-directory-msi-app-service",
-    options: {},
-  },
-};
 
 app.serviceBusQueue("receiveTestEvent", {
   queueName: "jaytest-events",
@@ -21,16 +8,6 @@ app.serviceBusQueue("receiveTestEvent", {
     message: unknown,
     context: InvocationContext
   ): Promise<void> => {
-    context.log("Received message", message);
-
-    const pool = await sql.connect(sqlConfig);
-
-    await pool.request().query(`
-      UPDATE dbo.updates
-      SET
-        value = value + 1,
-        last_updated = SYSUTCDATETIME()
-      WHERE id = 1;
-    `);
+    context.log("Received message:", JSON.stringify(message));
   },
 });
