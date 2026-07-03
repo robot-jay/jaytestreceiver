72a879d74b18dddccc64f87e36d950e03f58916d
Jay Ragsdale <jay@mtstesters.com>
2026-07-03 14:26:45 -0500

    Added debug logging.

--- src/functions/receiveTestEvent.ts
+++ src/functions/receiveTestEvent.ts
@@ -1,4 +1,17 @@
 import { app, InvocationContext } from "@azure/functions";
+import sql from "mssql";
+
+const sqlConfig: sql.config = {
+  server: process.env.SQL_SERVER!,
+  database: process.env.SQL_DATABASE!,
+  options: {
+    encrypt: true,
+  },
+  authentication: {
+    type: "azure-active-directory-msi-app-service",
+    options: {},
+  },
+};
 
 app.serviceBusQueue("receiveTestEvent", {
   queueName: "jaytest-events",
@@ -8,6 +21,22 @@ app.serviceBusQueue("receiveTestEvent", {
     message: unknown,
     context: InvocationContext
   ): Promise<void> => {
-    context.log("Received message:", JSON.stringify(message));
+    context.log("Received message", message);
+
+    const pool = await sql.connect(sqlConfig);
+
+    try {
+        const pool = await sql.connect(sqlConfig);
+
+        await pool.request().query(`
+            UPDATE dbo.updates
+            SET value = value + 1,
+                last_updated = SYSUTCDATETIME()
+            WHERE id = 1;
+        `);
+    } catch (error) {
+        context.error("SQL update failed", error);
+        throw error;
+    }
   },
 });
